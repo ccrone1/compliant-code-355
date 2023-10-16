fc1726399e541bd154f8d6144a428bd87f8529e3
ccrone1 <131101714+ccrone1@users.noreply.github.com>
2023-10-15 23:15:38 -0500

    Update OOP57.cpp

--- compliant-code-355/copliant_code/OOOP57/OOP57.cpp
+++ compliant-code-355/copliant_code/OOOP57/OOP57.cpp
@@ -10,13 +10,15 @@ class ValueClass{
     
 public:
     ValueClass() : value(0) {}
+
+    ValueClass(int valueGiven) : value(valueGiven) {}
     
     /**
      * @brief Overloaded = operator to reset an object of type ValueClass
      */
     ValueClass& operator=(const ValueClass& copy) {
         if (this != &copy) {
-            value = 0; // Reset the value
+            value = copy.value;
         }
         return *this;
     }
@@ -27,22 +29,13 @@ public:
     int getValue() const {
         return value;
     }
-    
-    /**
-     * @brief Constructor to initialize the Rectangle.
-     * @param setValue and integer which will make the value of the ValueClass the input value
-     */
-    void setValue(int setValue) {
-        value = setValue;
-    }
 };
 
 int main() {
-    ValueClass value1;
-    value1.setValue(20); //sets the value to 20
-    std::cout << "Value: " << value1.getValue() << endl; //should be 20
-    value1 = ValueClass(); // Reinitialize value1 using the overloaded assignment operator which is compliant since we are not using any memory copies of a new ValueClass object
-    std::cout << "Value: " << value1.getValue() << endl; //shoudl have copied the value 0 from the constructor
+    ValueClass value1(20); //sets the value to 20
+    cout << "Value: " << value1.getValue() << endl; //should be 20
+    value1 = ValueClass(10); // Reinitialize value1 using the overloaded assignment operator which is compliant since we are not using any memory copies of a new ValueClass object
+    cout << "Value: " << value1.getValue() << endl; //should have copied the value 0 from the constructor
     
     return 0;
 }
